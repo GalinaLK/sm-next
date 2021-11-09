d859dda5fde5efeb4a4c53bbc0d79a2beab384d6
GalinaLK <gangnam0803@gmail.com>
2021-11-09 15:50:52 +0200

    garbige teamviewer

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,9 @@ import '../styles/globals.css'
 import Layout from '../components/Layout'
 import { Provider } from 'react-redux'
 import { useStore } from '../store'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
 
 export default function App({ Component, pageProps }) {
   const store = useStore(pageProps.initialReduxState)
